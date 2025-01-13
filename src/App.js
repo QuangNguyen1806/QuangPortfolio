70672613490fca70bb5bb32a52ca67c0b1e9dbbe
QuangNguyen1806 <qhn210@nyu.edu>
2025-01-12 23:54:56 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.scss'
 import Layout from './components/Layout'
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 
 function App() {
   return (
-    <HashRouter>
-        <HashRouter path="/" element={<Layout />}>
-          <HashRouter index element={<Home />} />
-          <HashRouter path="contact" element={<Contact />} />
-          <HashRouter path="about" element={<About />} />
-      </HashRouter>
-      </HashRouter>
+    <Routes>
+        <Route path="" element={<Layout />}>
+          <Route index element={<Home />} path = 'https://quangnguyen1806.github.io/QuangPortfolio/'/>
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+      </Route>
+      </Routes>
   )
 }
 
