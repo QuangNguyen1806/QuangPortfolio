66e7620566849a56b7c1a01ffb487cfcdfb45b5f
QuangNguyen1806 <qhn210@nyu.edu>
2025-01-13 00:06:39 -0500

    push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 
-function App()break {
+function App() {
   return (
     <Routes> 
         <Route path="" element={<Layout />}>
